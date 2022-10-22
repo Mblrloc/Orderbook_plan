50f4da438c59fc5cfd3c4f17cf78ec16db0068b1
Balgabaev Dmitry <44303408+Mblrloc@users.noreply.github.com>
2022-10-22 15:01:32 +0300

    Changed "blockhain" structure and logging

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,14 @@
+import logging
+
+
 class Blockchain:
     INCREASE = 0
     DECREASE = 1
+
+    def __new__(cls):  # singleton init
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Blockchain, cls).__new__(cls)
+        return cls.instance
 
     def __init__(self):
         self.balance_base = {}
@@ -13,7 +21,7 @@ class Blockchain:
             user_balance = self.balance_base[user]
             self.change_balance(user_balance, currency, qnt, mode)
         else:
-            raise Exception("blockchain", "no user")
+            logging.info("blockchain: no such user")
 
     @staticmethod
     def change_value(user_balance, currency, qnt, mode):
@@ -26,4 +34,8 @@ class Blockchain:
             if currency in user_balance and user_balance[currency] >= qnt:
                 user_balance[currency] -= qnt
             else:
-                raise Exception("blockchain", "not enough currency")
+                logging.info("blockchain", "not enough currency")
+
+    def transfer(self, user_from, user_to, currency, qnt):
+        self.change_balance(user_from, currency, qnt, mode=Blockchain.DECREASE)
+        self.change_balance(user_to, currency, qnt, mode=Blockchain.INCREASE)
--- smart_contract.py
+++ smart_contract.py
@@ -1,22 +1,31 @@
+import blockchain
+import logging
+import time
+
+
 class SmartContract:
     MAKER = 0
     TAKER = 1
 
-    def __init__(self, blockchain, u_id, maker, taker, parr_A, parr_B, qnt_A, qnt_B):
-        self.id = u_id
+    def __init__(self, u_id, maker, taker, parr_A, parr_B, qnt_A, qnt_B, exp_date=0):
+        self.u_id = u_id
         self.maker = maker
         self.taker = taker
         self.parr_A = parr_A
         self.parr_B = parr_B
         self.qnt_A = qnt_A
         self.qnt_B = qnt_B
-        self.balance = {}
         self.history = {}
-        self.blockhain = blockchain
+        self.exp_date = exp_date
+        self.blockchain = blockchain.Blockchain()
+        self.active = True
 
-    def entry_point(self, user, currency, value):
-        self.balance[currency] = self.balance.get(currency, 0) + value
-        self.history[user][currency] = self.history.get(user, {currency: value}).get(currency, 0) + value
+    def entry_point(self, user, currency, qnt):
+        if qnt >= 0:
+            self.blockchain.transfer(user, self.u_id, currency, qnt)
+            self.history[user][currency] = self.history.get(user, {currency: qnt}).get(currency, 0) + qnt
+        else:
+            logging.info("ep: negative qnt of ep tokens")
 
     def check_user(self, mode):
         if mode == SmartContract.MAKER:
@@ -24,10 +33,36 @@ class SmartContract:
         elif mode == SmartContract.TAKER:
             return self.history[self.taker][self.parr_B] >= self.qnt_B
         else:
-            raise Exception("sc", "err")
+            raise Exception("sc", "syntax error")
+
+    def check_time(self):
+        return self.exp_date == 0 or time.time() < self.exp_date
+
+    def check_all(self):
+        if ~self.active:
+            logging.info("execution: sc is cancelled")
+            return False
+        if ~self.check_time():
+            logging.info("execution: contract is expired")
+            return False
+        if ~(self.check_user(SmartContract.MAKER)):
+            logging.info("execution: not enough currency for maker")
+            return False
+        if ~(self.check_user(SmartContract.TAKER)):
+            logging.info("execution: not enough currency for taker")
+            return False
+        return True
 
     def execute(self):
-        if ~(self.check_user(SmartContract.MAKER)):
-            raise Exception("exe", "not enough currency for maker")
-        if ~(self.check_user(SmartContract.TAKER)):
-            raise Exception("exe", "not enough currency for taker")
+        if self.check_all():
+            self.blockchain.transfer(self.u_id, self.maker, self.parr_B, self.qnt_B)
+            self.blockchain.transfer(self.u_id, self.taker, self.parr_A, self.qnt_A)
+
+    def cancel(self):
+        qnt_a = self.history[self.maker][self.parr_A]
+        qnt_b = self.history[self.taker][self.parr_B]
+        if qnt_a > 0:
+            self.blockchain.transfer(self.u_id, self.maker, self.parr_A, qnt_a)
+        if qnt_b > 0:
+            self.blockchain.transfer(self.u_id, self.taker, self.parr_B, qnt_b)
+        self.active = False  # may vary in actual realisation
